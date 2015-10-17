7244b4648a551ad389369e79e0b5a4d98cda5d85
Aakash Goel <aakash.lpin@gmail.com>
2015-10-18 04:31:42 +0530

    handle Installation in the backend

--- App/apis/API.js
+++ App/apis/API.js
@@ -3,6 +3,24 @@ var API = {
     email = email.toLowerCase().trim();
     return fetch(`https://cheapass.in/api/dashboard/tracks/${email}`)
     .then((response) => response.json());
+  },
+
+  requestAppInstallation (data) {
+    return fetch(`https://cheapass.in/mobile/register/ios`, {
+      credentials: 'same-origin',
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(response => {
+      console.log('requestAppInstallation success ', response);
+      return response;
+    })
+    .catch(e => console.log('error hitting /mobile/register/ios ', e));
   }
 };
 
